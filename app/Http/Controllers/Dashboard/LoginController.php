4df79de0ad310bf066338797ee703156d0ba1aec
Muhammad Yousif <muhammadaboidrees@yahoo.com>
2022-12-28 09:30:29 +0300

    Settings and authentication redirect

--- app/Http/Controllers/Dashboard/LoginController.php
+++ app/Http/Controllers/Dashboard/LoginController.php
@@ -4,27 +4,49 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AdminLoginRequest;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 
 class LoginController extends Controller
 {
   //
-  public function index()
+  public function index(): Factory|View|Application
   {
     return view('dashboard.auth.login');
   }
 
-  public function login(AdminLoginRequest $request)
+  public function login(AdminLoginRequest $request): RedirectResponse
   {
 
-    $remember_me = $request->has('remember_me') ? true : false;
     $credentials = $request->only(['email', 'password']);
 
-    $auth = auth()->guard('admin')->attempt($credentials, $remember_me);
-    if ($auth) {            //  notify()->success("تم الدخول بنجاح");
+    $auth = auth()->guard('admin')->attempt($credentials, $request->has('remember_me'));
+
+    if ($auth) {
+      //  notify()->success("تم الدخول بنجاح");
       return redirect()->route('admin.dashboard');
     }
 
     return redirect()->back()->with(['error' => 'خطأ في بيانات الدخول']);
+  }
+
+
+  public function logout()
+  {
+    $guard = $this->getGuard();
+    $guard->logout();
+
+
+    return redirect()->route('admin.login');
+  }
+
+  protected function getGuard(): Guard|StatefulGuard|Application|Factory
+  {
+    return auth('admin');
   }
 }
